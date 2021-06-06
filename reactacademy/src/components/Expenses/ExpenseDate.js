76bdec902e94a8953cb57ff789bf7979ea56b9e3
Emanuel <lejiei@gmail.com>
2021-06-06 01:24:44 -0300

    organized folders

--- reactacademy/src/components/Expenses/ExpenseDate.js
+++ reactacademy/src/components/Expenses/ExpenseDate.js
@@ -1,4 +1,4 @@
-import './ExpenseDate.css';
+import '../UI/ExpenseDate.css';
 function ExpenseDate(props){
   const month = props.date.toLocaleString('en-US', {month:'long'});
   const day = props.date.toLocaleString('en-US', {day:'2-digit'});
